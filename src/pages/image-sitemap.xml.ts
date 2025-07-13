1e9fd26822ccb7802dfd5f10d85e7056f6a4bc9d
nakuwalar <nakuwalar@gmail.com>
2025-07-14 02:31:49 +0700

    Update image-sitemap.xml.ts

--- src/pages/image-sitemap.xml.ts
+++ src/pages/image-sitemap.xml.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 import { slugify } from '../utils/slugify';
 import { getAllVideos, type VideoData } from '../utils/data';
-import { terbit } from '../utils/site';
+import { terbit, nama } from '../utils/site';
 
 export const GET: APIRoute = async ({ site }) => {
   if (!site) {
@@ -47,6 +47,9 @@ export const GET: APIRoute = async ({ site }) => {
       ? thumbnailUrl
       : `${baseUrl}${thumbnailUrl}`;
 
+    const imageCaptionDescription = `Nonton streaming video bokep viral ${video.title} yang terbaru di link ${nama} kategori ${video.category}`;
+
+
     if (absoluteThumbnailUrl && videoDetailUrl) {
       const videoLastMod = video.dateModified || video.datePublished || defaultPublishedDate;
 
@@ -56,7 +59,7 @@ export const GET: APIRoute = async ({ site }) => {
           <lastmod>${videoLastMod}</lastmod>
           <image:image>
             <image:loc>${absoluteThumbnailUrl}</image:loc>
-            <image:caption>${escapeXml(video.description || video.title)}</image:caption>
+            <image:caption>${escapeXml(imageCaptionDescription)}</image:caption>
             <image:title>${escapeXml(video.title)}</image:title>
           </image:image>
         </url>
